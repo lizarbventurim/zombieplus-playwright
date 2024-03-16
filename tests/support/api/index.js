c3a279fa2047a988ff6e33d23303b03fe9c99f49
lizarbventurim <lizarbpacheco@gmail.com>
2024-03-16 13:31:46 -0300

    feat: config dotenv in requests api

--- tests/support/api/index.js
+++ tests/support/api/index.js
@@ -1,15 +1,17 @@
+require('dotenv').config();
 const { expect } = require('@playwright/test');
 
 
 export class Api {
 
     constructor(request) {
+        this.baseApi = process.env.BASE_API;
         this.request = request;
         this.token = undefined
     }
 
     async setToken() {
-        const response = await this.request.post('http://localhost:3333/sessions', {
+        const response = await this.request.post(this.baseApi + '/sessions', {
             data: {
                 email: 'admin@zombieplus.com',
                 password: 'pwd123'
@@ -22,7 +24,7 @@ export class Api {
     }
 
     async getCompanyIdByName(companyName) {
-        const response = await this.request.get('http://localhost:3333/companies', {
+        const response = await this.request.get(this.baseApi + '/companies', {
             headers: {
                 Authorization: this.token,
             },
@@ -42,7 +44,7 @@ export class Api {
     async postMovie(movie) {
 
         const companyId = await this.getCompanyIdByName(movie.company);
-        const response = await this.request.post('http://localhost:3333/movies', {
+        const response = await this.request.post(this.baseApi + '/movies', {
             headers: {
                 Authorization: this.token,
                 ContentType: 'multipart/form-data',
