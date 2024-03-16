4da9a395c5523547c4917ad25aa35fc75243b01f
lizarbventurim <lizarbpacheco@gmail.com>
2024-03-16 14:47:40 -0300

    feat: format document

--- tests/e2e/login.spec.js
+++ tests/e2e/login.spec.js
@@ -1,43 +1,38 @@
-const { test, expect } = require('../support');
-const username = 'Admin';
+const { test, expect } = require("../support");
+const username = "Admin";
 
 test("Deve acessar a página administrativa", async ({ page }) => {
-    await page.login.do('admin@zombieplus.com', 'pwd123', username);
+    await page.login.do("admin@zombieplus.com", "pwd123", username);
 });
 
 test("Não deve acessar com a senha incorreta", async ({ page }) => {
-
     await page.login.visit();
-    await page.login.submitLoginForm('admin@zombieplus.com', 'pwd1234');
-    const message = 'Ocorreu um erro ao tentar efetuar o login. Por favor, verifique suas credenciais e tente novamente.';
+    await page.login.submitLoginForm("admin@zombieplus.com", "pwd1234");
+    const message =
+        "Ocorreu um erro ao tentar efetuar o login. Por favor, verifique suas credenciais e tente novamente.";
     await page.popup.haveText(message);
-
 });
 
 test("Não deve acessar quando o email  é inválido", async ({ page }) => {
     await page.login.visit();
-    await page.login.submitLoginForm('teste.com.bw', 'pwd1234');
-    await page.login.alertHaveText('Email incorreto');
-
+    await page.login.submitLoginForm("teste.com.bw", "pwd1234");
+    await page.login.alertHaveText("Email incorreto");
 });
 
 test("Não deve acessar quando o email não é preenchido", async ({ page }) => {
     await page.login.visit();
-    await page.login.submitLoginForm('', 'pwd1234');
-    await page.login.alertHaveText('Campo obrigatório');
-
+    await page.login.submitLoginForm("", "pwd1234");
+    await page.login.alertHaveText("Campo obrigatório");
 });
 
 test("Não deve acessar quando a senha não é preenchida", async ({ page }) => {
     await page.login.visit();
-    await page.login.submitLoginForm('teste@teste.com.br', '');
-    await page.login.alertHaveText('Campo obrigatório');
-
+    await page.login.submitLoginForm("teste@teste.com.br", "");
+    await page.login.alertHaveText("Campo obrigatório");
 });
 
 test("Não deve acessar quando nenhum campo é preenchido", async ({ page }) => {
     await page.login.visit();
-    await page.login.submitLoginForm('', '');
-    await page.login.alertHaveText(['Campo obrigatório', 'Campo obrigatório']);
-
+    await page.login.submitLoginForm("", "");
+    await page.login.alertHaveText(["Campo obrigatório", "Campo obrigatório"]);
 });
