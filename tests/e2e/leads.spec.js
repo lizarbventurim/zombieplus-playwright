fb6af39697d84b42f785c34de31806fa60a8fcdd
lizarbventurim <lizarbpacheco@gmail.com>
2024-03-16 09:12:26 -0300

    refactor: alter components used and message

--- tests/e2e/leads.spec.js
+++ tests/e2e/leads.spec.js
@@ -3,8 +3,8 @@ const { test } = require('../support');
 const { faker } = require('@faker-js/faker');
 
 //variables
-const message = "Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato!";
-const registredEmail = 'O endereço de e-mail fornecido já está registrado em nossa fila de espera.';
+const message = "Agradecemos por compartilhar seus dados conosco. Em breve, nossa equipe entrará em contato.";
+const registredEmail = 'Verificamos que o endereço de e-mail fornecido já consta em nossa lista de espera. Isso significa que você está um passo mais perto de aproveitar nossos serviços.';
 const requiredFiledMessage = 'Campo obrigatório';
 let leadName;
 let leadEmail;
@@ -22,7 +22,7 @@ test('Deve cadastrar um lead na fila de espera ', async ({ page }) => {
   await page.leads.visit();
   await page.leads.openLeadModal();
   await page.leads.submitLeadForm(leadName, leadEmail);
-  await page.toast.containText(message);
+  await page.popup.haveText(message);
 
 
 });
@@ -32,7 +32,7 @@ test('Não deve cadastrar um lead na fila de espera quando o email já tiver cad
   await page.leads.visit();
   await page.leads.openLeadModal();
   await page.leads.submitLeadForm(leadName, leadEmail);
-  await page.toast.containText(registredEmail);
+  await page.popup.haveText(registredEmail);
 
 
 });
